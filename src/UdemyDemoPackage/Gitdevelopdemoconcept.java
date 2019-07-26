56868866fc69acf7d89a810d0e5812d8dd4a8e7d
Sridevi <sridevi.mittimeeda@gmail.com>
2019-07-26 16:36:31 +0530

    Development file added

---
+++ src/UdemyDemoPackage/Gitdevelopdemoconcept.java
@@ -0,0 +1,10 @@
+package UdemyDemoPackage;
+
+public class Gitdevelopdemoconcept {
+
+	public static void main(String[] args) {
+		 System.out.println("Changes made to development branch");
+
+	}
+
+}
